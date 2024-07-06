9cf565ff0cd811afdc2f711ee1e7b0931fcf589f
bulhoesgabriel <bulhoesgabriel@outlook.com>
2024-07-05 21:19:57 -0300

    updates

--- meu-projeto/src/components/Evento.js
+++ meu-projeto/src/components/Evento.js
@@ -1,13 +1,20 @@
-function Evento({numero}) {
+import Button from './Button'
+
+function Evento() {
 
     function meuEvento() {
-        console.log(`Opa, fui ativado! ${numero}`)
+        console.log('Ativando primeiro evento')
     }
 
-    return(
+    function segundoEvento() {
+        console.log('Ativando segundo evento')
+    }
+
+    return (
         <>
             <p>Clique para disparar um evento:</p>
-            <button onClick={meuEvento}>Ativar!</button>
+            <Button event={meuEvento} text="Primeiro evento" />
+            <Button event={segundoEvento} text="Segundo evento" />
         </>
     )
 
